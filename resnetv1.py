4cb2dbefa1c8ac71025c19f342e26533e1f896f5
Brokolito <m.castro14@ufromail.cl>
2025-05-04 12:41:58 -0400

    Se arreglan archivos py, estos fueron utilizados en khipu

--- resnetv1.py
+++ resnetv1.py
@@ -127,23 +127,25 @@ def resnet_layer(inputs,num_filters=16,kernel_size=3,strides=1,activation='relu'
         x = conv(x)
     return x
 
-def resnet_v2(input_shape, depth, num_classes=10):
-    """ResNet Version 2 Model builder [b]
-    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or 
-    also known as bottleneck layer.
-    First shortcut connection per layer is 1 x 1 Conv2D.
-    Second and onwards shortcut connection is identity.
-    At the beginning of each stage, 
-    the feature map size is halved (downsampled)
-    by a convolutional layer with strides=2, 
-    while the number of filter maps is
-    doubled. Within each stage, the layers have 
-    the same number filters and the same filter map sizes.
+def resnet_v1(input_shape, depth, num_classes=10):
+    """ResNet Version 1 Model builder [a]
+    Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+    Last ReLU is after the shortcut connection.
+    At the beginning of each stage, the feature map size is halved
+    (downsampled) by a convolutional layer with strides=2, while 
+    the number of filters is doubled. Within each stage, 
+    the layers have the same number filters and the
+    same number of filters.
     Features maps sizes:
-    conv1  : 32x32,  16
-    stage 0: 32x32,  64
-    stage 1: 16x16, 128
-    stage 2:  8x8,  256
+    stage 0: 32x32, 16
+    stage 1: 16x16, 32
+    stage 2:  8x8,  64
+    The Number of parameters is approx the same as Table 6 of [a]:
+    ResNet20 0.27M
+    ResNet32 0.46M
+    ResNet44 0.66M
+    ResNet56 0.85M
+    ResNet110 1.7M
     Arguments:
         input_shape (tensor): shape of input image tensor
         depth (int): number of core convolutional layers
@@ -151,69 +153,51 @@ def resnet_v2(input_shape, depth, num_classes=10):
     Returns:
         model (Model): Keras model instance
     """
-    if (depth - 2) % 9 != 0:
-        raise ValueError('depth should be 9n+2 (eg 110 in [b])')
+    if (depth - 2) % 6 != 0:
+        raise ValueError('depth should be 6n+2 (eg 20, 32, in [a])')
+    
     # start model definition.
-    num_filters_in = 16
-    num_res_blocks = int((depth - 2) / 9)
+    num_filters = 16
+    num_res_blocks = int((depth - 2) / 6)
 
     inputs = Input(shape=input_shape)
+    x = resnet_layer(inputs=inputs)
     
-    # v2 performs Conv2D with BN-ReLU
-    # on input before splitting into 2 paths
-    x = resnet_layer(inputs=inputs,num_filters=num_filters_in,conv_first=True)
-
     # instantiate the stack of residual units
-    for stage in range(3):
+    for stack in range(3):
         for res_block in range(num_res_blocks):
-            activation = 'relu'
-            batch_normalization = True
             strides = 1
-            if stage == 0:
-                num_filters_out = num_filters_in * 4
-                # first layer and first stage
-                if res_block == 0:  
-                    activation = None
-                    batch_normalization = False
-            else:
-                num_filters_out = num_filters_in * 2
-                # first layer but not first stage
-                if res_block == 0:
-                    # downsample
-                    strides = 2 
-
-            # bottleneck residual unit
-            y = resnet_layer(inputs=x,num_filters=num_filters_in,kernel_size=1,strides=strides,
-                             activation=activation,batch_normalization=batch_normalization,conv_first=False)
-            y = resnet_layer(inputs=y,num_filters=num_filters_in,conv_first=False)
-            y = resnet_layer(inputs=y,num_filters=num_filters_out,kernel_size=1,conv_first=False)
-            if res_block == 0:
-                # linear projection residual shortcut connection
-                # to match changed dims
-                x = resnet_layer(inputs=x,num_filters=num_filters_out,kernel_size=1,
-                                 strides=strides,activation=None,batch_normalization=False)
+            # first layer but not first stack
+            if stack > 0 and res_block == 0:  
+                strides = 2  # downsample
+            y = resnet_layer(inputs=x,num_filters=num_filters,strides=strides)
+            y = resnet_layer(inputs=y,num_filters=num_filters,activation=None)
+            
+            # first layer but not first stack
+            if stack > 0 and res_block == 0:
+                # linear projection residual shortcut
+                # connection to match changed dims
+                x = resnet_layer(inputs=x,num_filters=num_filters,kernel_size=1,strides=strides,
+                                 activation=None,batch_normalization=False)
             x = add([x, y])
-
-        num_filters_in = num_filters_out
+            x = Activation('relu')(x)
+        num_filters *= 2
 
     # add classifier on top.
-    # v2 has BN-ReLU before Pooling
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    # v1 does not use BN after last shortcut connection-ReLU
     x = AveragePooling2D(pool_size=8)(x)
     y = Flatten()(x)
-    outputs = Dense(num_classes,activation='softmax',kernel_initializer='he_normal')(y)
+    outputs = Dense(num_classes,activation='softmax')(y)
 
     # instantiate model.
     model = Model(inputs=inputs, outputs=outputs)
     return model
 
 
-if version == 2:
-    model = resnet_v2(input_shape=input_shape, depth=depth)
-#else:
-#    model = resnet_v1(input_shape=input_shape, depth=depth)
+model = resnet_v1(input_shape=input_shape, depth=depth,num_classes=num_classes)
 
+model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=lr_schedule(0)),metrics=['acc'])
+model.summary()
 model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=lr_schedule(0)),metrics=['acc'])
 model.summary()
 
@@ -268,7 +252,7 @@ else:
 
     steps_per_epoch =  math.ceil(len(X_test) / batch_size)
     # fit the model on the batches generated by datagen.flow().
-    history=model.fit(x=datagen.flow(X_test, y_train, batch_size=batch_size),
+    history=model.fit(x=datagen.flow(X_train, y_train, batch_size=batch_size),
               verbose=2,epochs=epochs,validation_data=(X_test, y_test),
               steps_per_epoch=steps_per_epoch,callbacks=callbacks)
 
